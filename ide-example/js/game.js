fe2e594af06ce1fe7574627d60ec4025f382d655
StanZGenchev <stan.z.genchev@gmai.com>
2023-06-22 19:48:09 +0300

    transitioning to 0.29.4 wip 1

--- ide-example/js/game.js
+++ ide-example/js/game.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2021 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
+ * Copyright (c) 2010-2023 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
@@ -18,9 +18,10 @@ gameView.config(["messageHubProvider", function (messageHubProvider) {
 // Initialize controller
 gameView.controller('GameViewController', ['$scope', 'messageHub', function ($scope, messageHub) {
 
-    $scope.btnText = "State button";
+    $scope.btnText = "Badge button";
     $scope.btnType = "positive";
-    $scope.btnState = "disabled-focusable";
+    $scope.btnState = "";
+    $scope.btnToggled = false;
 
     $scope.splitButtonAction = "Default";
     $scope.segmentedModel = "middle";
@@ -226,11 +227,11 @@ gameView.controller('GameViewController', ['$scope', 'messageHub', function ($sc
     };
 
     $scope.state = function () {
-        if ($scope.btnState === "disabled-focusable")
-            $scope.btnState = 'selected';
-        else if ($scope.btnState === "selected")
-            $scope.btnState = '';
-        else $scope.btnState = "disabled-focusable";
+        $scope.btnState = "disabled-focusable";
+    };
+
+    $scope.toggle = function () {
+        $scope.btnToggled = !$scope.btnToggled;
     };
 
     $scope.steps = [
